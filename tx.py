42621ed68d1f9e2b3693c19c658e4d324bbdd2fa
narodnik <s>
2022-02-27 22:26:23 +0100

    basic skeleton structure, added parsing for add command

--- tx.py
+++ tx.py
@@ -1,49 +1,191 @@
 #!/usr/bin/python
 
 import argparse
+import datetime
+import pprint
 import requests
 import json
+import sys
+from decimal import Decimal as Real
+
+def error(message):
+    print(f"Error: {message}", file=sys.stderr)
+    sys.exit(-1)
+
+# Is this string an integer number
+def is_integer(s):
+    try:
+        _ = int(s)
+    except ValueError:
+        return False
+    return True
+
+def validate_due_date(date):
+    if date is None:
+        return True
+    
+    assert(isinstance(date, str))
+    if len(date) != 4:
+        return False
+
+    if not is_integer(date):
+        return False
+
+    try:
+        due_date = convert_due_date(date)
+    except ValueError:
+        return False
+
+    return True
+
+# This function MUST be called after validate_due_date(date)
+def convert_due_date(date):
+    if date is None:
+        return None
+
+    assert(isinstance(date, str))
+    assert(len(date) == 4)
+    assert(is_integer(date))
+
+    # We know the date is a 4 byte integer value.
+    # Now evaluate the month and year
+    # DDMM format
+    day, month = int(date[:2]), int(date[2:])
+    year = datetime.date.today().year
+    #print(f"{day} {month} {year}")
+
+    # !!!
+    # This line can throw ValueError if month or day is invalid!
+    # Should be caught by validate_due_date(date)
+    due_date = datetime.date(year, month, day)
+
+    return due_date
+
+class TaskInfo:
+
+    def __init__(self, assign, project, due, rank):
+        self.assign = assign
+        self.project = project
+        self.due = due
+        self.rank = rank
+
+    def __repr__(self):
+        return (
+            f"TaskInfo {{\n"
+            f"  assign: {self.assign},\n"
+            f"  project: {self.project},\n"
+            f"  due: {self.due},\n"
+            f"  rank: {self.rank},\n"
+            f"}}"
+        )
+
+def cmd_add(args, settings):
+    if not validate_due_date(args.due):
+        error(f"due date {args.due} is not valid")
+    due = convert_due_date(args.due)
+
+    # TODO: created_at attribute
+
+    task_info = TaskInfo(args.assign, args.project, due, args.rank)
+    print(f"{task_info}")
 
 def arg_parser(client):
-    parser = argparse.ArgumentParser(prog='dark',
-                                          usage='%(prog)s [commands]',
-                                          description="""DarkFi wallet
-                                          command-line tool""")
-    parser.add_argument("-k", "--key", action='store_true', help="Generate a new keypair")
-    parser.add_argument("-i", "--info", action='store_true', help="Request info from daemon")
-    parser.add_argument("-s", "--stop", action='store_true', help="Send a stop signal to the daemon")
-    parser.add_argument("-hi", "--hello", action='store_true', help="Say hello")
+    parser = argparse.ArgumentParser(prog='tx',
+        usage='%(prog)s [commands]',
+        description="Collective task management cli"
+    )
+
+    subparsers = parser.add_subparsers()
+
+    # add [-a/--assign USER] [-p/--project zk] [-d/--due DDMM] [-r/--rank 4.87]
+    #   [-c/--custom CUSTOM_KEY:CUSTOM_VALUE]...
+    # if assigned USER is empty, then task is unassigned
+    # custom adds additional custom attributes
+    # rank is arbitrary Decimal precision
+    #
+    # $ tx add -a nar -p zk
+    # Title: "read paper on DARK compilers"
+    # Description: _
+    #
+    # By default just prompt input, or you can set the program to open with
+    # a custom editor
+    parser_add = subparsers.add_parser("add", help="add a new task")
+    parser_add.add_argument(
+        "-a", "--assign", #action='store_true',
+        default=None,
+        help="assign task to user")
+    parser_add.add_argument(
+        "-p", "--project", #action='store_true',
+        default=None,
+        help="task project (can be hierarchical: crypto.zk")
+    parser_add.add_argument(
+        "-d", "--due", #action='store_true',
+        default=None,
+        help="due date: 0222")
+    parser_add.add_argument(
+        "-r", "--rank", #action='store_true',
+        type=Real, default=None,
+        help=("project rank, is an arbitrary precision"
+              "decimal real value: 4.8761"))
+    parser_add.add_argument(
+        "-c", "--custom",
+        default=None,
+        help="custom_key:custom_value attribute")
+    parser_add.set_defaults(func=cmd_add)
+
+    #parser.add_argument("-k", "--key", action='store_true', help="Generate a new keypair")
+    #parser.add_argument("-i", "--info", action='store_true', help="Request info from daemon")
+    #parser.add_argument("-s", "--stop", action='store_true', help="Send a stop signal to the daemon")
+    #parser.add_argument("-hi", "--hello", action='store_true', help="Say hello")
     args = parser.parse_args()
 
-    if args.key:
-        try:
-            print("Attemping to generate a new key pair...")
-            client.key_gen(client.payload)
-        except Exception:
-            raise
+    # TODO: load config, only steps until #2 for now. Do #3 onwards later
+    # weaker priority than command line args
+    # ~/.config/tx/tx.toml
+    # [settings]
+    # custom_editor="nvim"
+    #
+    # 1. Read environment variable TX_CONFIG_PATH=...
+    #    default is not set is ~/.config/tx/
+    # 2. make directory if it doesn't already exist
+    # 3. Load {config_path}/tx.toml
+    #    has a settings section
+    #       [settings]
+    #       custom_editor="nvim"    # Forget this for now
+    # 4. Load config into Settings class
+    settings = None
 
-    if args.info:
-        try:
-            print("Info was entered")
-            client.get_info(client.payload)
-            print("Requesting daemon info...")
-        except Exception:
-            raise
+    args.func(args, settings)
 
-    if args.stop:
-        try:
-            print("Stop was entered")
-            client.stop(client.payload)
-            print("Sending a stop signal...")
-        except Exception:
-            raise
+    #if args.key:
+    #    try:
+    #        print("Attemping to generate a new key pair...")
+    #        client.key_gen(client.payload)
+    #    except Exception:
+    #        raise
 
-    if args.hello:
-        try:
-            print("Hello was entered")
-            client.say_hello(client.payload)
-        except Exception:
-            raise
+    #if args.info:
+    #    try:
+    #        print("Info was entered")
+    #        client.get_info(client.payload)
+    #        print("Requesting daemon info...")
+    #    except Exception:
+    #        raise
+
+    #if args.stop:
+    #    try:
+    #        print("Stop was entered")
+    #        client.stop(client.payload)
+    #        print("Sending a stop signal...")
+    #    except Exception:
+    #        raise
+
+    #if args.hello:
+    #    try:
+    #        print("Hello was entered")
+    #        client.say_hello(client.payload)
+    #    except Exception:
+    #        raise
 
 
 class DarkClient:
@@ -97,6 +239,7 @@ class DarkClient:
 if __name__ == "__main__":
     client = DarkClient()
     arg_parser(client)
+    sys.exit(0)
 
     #rpc()
     ## Example echo method
@@ -114,6 +257,4 @@ if __name__ == "__main__":
     #print(response)
     #assert response["result"] == "Hello World!"
     #assert response["jsonrpc"]
-
-
 
